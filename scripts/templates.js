87b10dae1240250275cc9f787a63f9db9777079b
Ivan Kravets <me@ikravets.com>
2014-11-13 15:15:40 +0200

    Fix Angular App name

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,4 +1,4 @@
-angular.module('app').run(['$templateCache', function($templateCache) {
+angular.module('siteApp').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('views/404.html',

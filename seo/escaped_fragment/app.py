e52dd45ffda0f7139f7a07470e18f872e4507540
Ivan Kravets <me@ikravets.com>
2014-12-06 01:19:29 +0200

    Avoid PhantomJS craching

--- seo/escaped_fragment/app.py
+++ seo/escaped_fragment/app.py
@@ -1,7 +1,7 @@
 # Copyright (C) Ivan Kravets <me@ikravets.com>
 # See LICENSE for details.
 
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 from urllib import unquote
 
 
@@ -14,13 +14,27 @@ def application(env, start_response):
         status = "500 Internal Server Error"
     else:
         url = "http://platformio.ikravets.com/#!" + unquote(qs[19:])
-        response = check_output([
-            "phantomjs", "--disk-cache=true", "--load-images=false",
-            "crawler.js", url
-        ])
-        if "404 Not Found" in response:
-            status = "404 Not Found"
+        try:
+            response = get_webcontent(url)
+            if "404 Not Found" in response:
+                status = "404 Not Found"
+        except Exception:
+            status = "500 Internal Server Error"
 
     start_response(status, [("Content-Type", "text/html"),
                             ("Content-Length", str(len(response)))])
     return response
+
+
+def get_webcontent(url):
+    retrynums = 0
+    while retrynums < 3:
+        try:
+            response = check_output([
+                "phantomjs", "--disk-cache=true", "--load-images=false",
+                "crawler.js", url
+            ])
+            return response
+        except CalledProcessError:
+            retrynums += 1
+    raise Exception("Could not retrieve content from %s" % url)
